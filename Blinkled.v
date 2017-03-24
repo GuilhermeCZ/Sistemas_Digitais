5133f0683842a2f775423c8195722ef920f96501
Guilherme Caon Z <guilhermecaonzambonin@hotmail.com>
2017-03-24 20:07:32 -0300

    now works in gtk

--- Blinkled.v
+++ Blinkled.v
@@ -28,3 +28,56 @@ module led (
     assign  LEDG[0] <= state;
 
 endmodule //
+
+/*module contador (
+    input clk,
+    output state);
+
+    reg [28:0]count = 0;
+    reg [1:0] lig = 0;
+
+    assign state = (lig);
+
+    always @ (clk ) begin
+      count <= count + 1;
+      if (count == 50) begin
+        lig <= ~lig;
+        count <= 0;
+      end
+    end
+
+endmodule //
+
+module led (
+    input CLOCK_50,
+    output LEDG);
+
+    wire state;
+
+    contador LED(CLOCK_50, state);
+
+    assign  LEDG = state;
+
+endmodule //
+
+module teste;
+	
+	reg clk;
+	wire saida;	
+
+	led lig(clk, saida);
+
+	always #2 begin
+		clk <= ~clk;
+	end
+	
+	initial begin 
+		$dumpvars(0, clk, lig);
+		#5;
+		clk <= 0;
+		#5;
+		#300;
+		$finish;
+	end
+
+endmodule // */
