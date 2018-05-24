9aaba0be0062577bfc696032305baa95689fa9bf
GuilhermeCZ <guilhermecaonzambonin@hotmail.com>
2018-05-24 17:25:51 -0300

    minor fixes
    just fixes to the code

--- Trabalho0 - Braile/braile.v
+++ Trabalho0 - Braile/braile.v
@@ -7,83 +7,83 @@ module alfabeto(
 	assign hexa = (h);
 
 	always @(posedge CLOCK_50) begin
-		if (let == 2'b000001) begin
-			h = 2'b1110111; //A
+		if (let == 6'b000001) begin
+			h = 7'b1110111; //A
 		end
-		else if (let == 2'b000011) begin
-			h = 2'b0011111; //B
+		else if (let == 6'b000011) begin
+			h = 7'b0011111; //B
 		end
-		else if (let == 2'b100001) begin
-			h = 2'b1001110; //C
+		else if (let == 6'b100001) begin
+			h = 7'b1001110; //C
  		end
-		else if (let == 2'b110001) begin
-			h = 2'b0111101; //D
+		else if (let == 6'b110001) begin
+			h = 7'b0111101; //D
 		end
-		else if (let == 2'b010001) begin
-			h = 2'b1001111; //E
+		else if (let == 6'b010001) begin
+			h = 7'b1001111; //E
 		end
-		else if (let == 2'b100011) begin
-			h = 2'b1000111; //F
+		else if (let == 6'b100011) begin
+			h = 7'b1000111; //F
 		end
-		else if (let == 2'b110011) begin
-			h = 2'b1111011; //G
+		else if (let == 6'b110011) begin
+			h = 7'b1111011; //G
 		end
-		else if (let == 2'b010011) begin
-			h = 2'b0110111; //H
+		else if (let == 6'b010011) begin
+			h = 7'b0110111; //H
 		end
-		else if (let == 2'b100010) begin
-			h = 2'b0000110; //I
+		else if (let == 6'b100010) begin
+			h = 7'b0000110; //I
 		end
-		else if (let == 2'b110010) begin
-			h = 2'b0111100; //J
+		else if (let == 6'b110010) begin
+			h = 7'b0111100; //J
 		end
-		else if (let == 2'b000101) begin
-			h = 2'b0101111; //K
+		else if (let == 6'b000101) begin
+			h = 7'b0101111; //K
 		end
-		else if (let == 2'b000111) begin
-			h = 2'b0001110; //L
+		else if (let == 6'b000111) begin
+			h = 7'b0001110; //L
 		end
-		else if (let == 2'b100101) begin
-			h = 2'b1110110; //M
+		else if (let == 6'b100101) begin
+			h = 7'b1110110; //M
 		end
-		else if (let == 2'b110101) begin
-			h = 2'b0010101; //N
+		else if (let == 6'b110101) begin
+			h = 7'b0010101; //N
 		end
-		else if (let == 2'b010101) begin
-			h = 2'b1111110; //O
+		else if (let == 6'b010101) begin
+			h = 7'b1111110; //O
 		end
-		else if (let == 2'b100111) begin
-			h = 2'b1100111; //P
+		else if (let == 6'b100111) begin
+			h = 7'b1100111; //P
 		end
-		else if (let == 2'b110111) begin
-			h = 2'b1110011; //Q
+		else if (let == 6'b110111) begin
+			h = 7'b1110011; //Q
 		end
-		else if (let == 2'b010111) begin
-			h = 2'b0000101; //R
+		else if (let == 6'b010111) begin
+			h = 7'b0000101; //R
 		end
-		else if (let == 2'b100110) begin
-			h = 2'b1011011; //S
+		else if (let == 6'b100110) begin
+			h = 7'b1011011; //S
 		end
-		else if (let == 2'b110110) begin
-			h = 2'b0001111; //T
+		else if (let == 6'b110110) begin
+			h = 7'b0001111; //T
 		end
-		else if (let == 2'b001101) begin
-			h = 2'b0011100; //U
+		else if (let == 6'b001101) begin
+			h = 7'b0011100; //U
 		end
-		else if (let == 2'b001111) begin
-			h = 2'b0111110; //V
+		else if (let == 6'b001111) begin
+			h = 7'b0111110; //V
 		end
-		else if (let == 2'b101101) begin
-			h = 2'b0000111; //X
+		else if (let == 'b101101) begin
+			h = 7'b0000111; //X
 		end
-		else if (let == 2'b111101) begin
-			h = 2'b0111011; //Y
+		else if (let == 6'b111101) begin
+			h = 7'b0111011; //Y
 		end
-		else if (let == 2'b011101) begin
-			h = 2'b1001001; //Z
+		else if (let == 6'b011101) begin
+			h = 7'b1001001; //Z
 		end
 		else begin
-			h = 2'b0000000; //0
+			h = 7'b0000000; //0
 		end
 	end
 	
@@ -93,35 +93,35 @@ endmodule
 module braile(
 	input CLOCK_50,
 	input [9:3] SW,
-	output [6:0] HEX);
+	output [6:0] HEX0);
 	
-	reg [5:0] letra = 2'b000000;
+	reg [5:0] letra = 6'b000000;
 	
 	alfabeto B(CLOCK_50, letra, hexa);
 	
-	assign HEX = hexa;
+	assign HEX0 = hexa;
 	
 	always @(posedge CLOCK_50) begin
 		if (~SW[9]) begin
-			letra = letra + 2'b100000;
+			letra = letra + 6'b100000;
 		end
 		else if (~SW[8]) begin
-			letra = letra + 2'b010000;
+			letra = letra + 6'b010000;
 		end
 		else if (~SW[7]) begin
-			letra = letra + 2'b001000;
+			letra = letra + 6'b001000;
 		end
 		else if (~SW[6]) begin
-			letra = letra + 2'b000100;
+			letra = letra + 6'b000100;
 		end
 		else if (~SW[5]) begin
-			letra = letra + 2'b000010;
+			letra = letra + 6'b000010;
 		end
 		else if (~SW[4]) begin
-			letra = letra + 2'b000001;
+			letra = letra + 6'b000001;
 		end
 		else begin
-			letra = 2'b000000;
+			letra = 6'b000000;
 		end
 	end
 	
