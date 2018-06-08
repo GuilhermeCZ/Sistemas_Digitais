274085c9a7b228d4f983fda4619835b41c40dde6
GuilhermeCZ <guilhermecaonzambonin@hotmail.com>
2018-06-08 19:52:29 -0300

    Revert "Revert "padilha files""
    This reverts commit a4d78515df5d809b51abd9fe51d417fd6e42d469.

---
+++ Padilha/desafiopadilha.py
@@ -0,0 +1,5 @@
+T = """Implementar um sistema para escrita e leitura na mem RAM, utilizando
+ teclado e um display. Como desafio implementar um programa para converter
+  um arquivo ASCII em um arquivo HEX compatível com a mem do LogiSim."""
+for c in T: print ('%x' % (ord(c)), end=" ")
+print()
