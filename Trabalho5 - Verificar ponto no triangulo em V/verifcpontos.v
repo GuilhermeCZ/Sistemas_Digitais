293d97ca0cf41fd90c56e06c35d5b16e437dc869
Guilherme Caon Z <guilhermecaonzambonin@hotmail.com>
2018-06-14 21:10:51 -0300

    now works
    now this thing works

--- Trabalho5 - Verificar ponto no triangulo em V/verifcpontos.v
+++ Trabalho5 - Verificar ponto no triangulo em V/verifcpontos.v
@@ -1,70 +1,101 @@
-module area (input clock, input [11:0] x1, input [11:0] x2, input [11:0] x3,
-          input [11:0] y1, input [11:0] y2, input [11:0] y3, output [11:0] abs);
+module sinal(
+    input [11:0] p1X,
+    input [11:0] p1Y,
+    input [11:0] p2X,
+    input [11:0] p2Y,
+    input [11:0] pToX,
+    input [11:0] pToY,
+    
+    output s
+);
 
-          reg [11:0] S0 = 0;
-          assign S0 = abs;
+    wire signed [11:0] sub1;
+    wire signed [11:0] sub2;
+    wire signed [11:0] sub3;
+    wire signed [11:0] sub4;
+    wire signed [22:0] sub5;
 
-          always @ ( posedge clock ) begin
-            S0 = ((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0);
-          end
+    wire signed [22:0] mul1;
+    wire signed [22:0] mul2;
 
-endmodule //area
-module isInside (input clock, input [11:0] x1, input [11:0] x2, input [11:0] x3,
-          input [11:0] y1, input [11:0] y2, input [11:0] y3,
-          input [11:0] x, input [11:0] y, output [1:0] ponto);
+    assign sub1 = pToX - p2X;
+    assign sub2 = p1Y - p2Y;
+    assign sub3 = p1X - p2X;
+    assign sub4 = pToY - p2Y;
+    assign mul1 = sub1 * sub2;
+    assign mul2 = sub3 * sub4;
+    assign sub5 = mul1 - mul2;
 
-          reg [1:0] S1 = 0;
-          reg [11:0] A0 = 0;
-          reg [11:0] A1 = 0;
-          reg [11:0] A2 = 0;
-          reg [11:0] A3 = 0;
-          assign S1 = ponto;
+    assign s = (sub5 >= 0) ? 1 : 0;
+endmodule
 
-          wire [11:0] SA;
-          wire [11:0] SA1;
-          wire [11:0] SA2;
-          wire [11:0] SA3;
 
-          area a(clock, x1, y1, x2, y2, x3, y3, SA);
-          area a1(clock, x, y, x2, y2, x3, y3, SA1);
-          area a2(clock, x1, y1, x, y, x3, y3, SA2);
-          area a1(clock, x1, y1, x2, y2, x, y, SA3);
+module testeTri(
+    input [11:0] p1X,
+    input [11:0] p1Y,
 
-          always @ ( posedge clock) begin
-            if (SA == SA1 + SA2 + SA3) begin
-              S1 = ~S1;
-            end
-            else begin
-              S1 = ~S1;
-            end
-          end
-endmodule //isInside
-module teste ();
-  reg clock;
-  reg [11:0] x1, x2, x3, y1, y2, y3;
-  wire [1:0] S0;
-  integer x;
-  integer y;
-  always #1 clock = ~clock;
+    input [11:0] p2X,
+    input [11:0] p2Y,
 
-  always @ ( posedge clock ) begin
-    for (x = 1; x <= 48; x++) begin
-      for (y = 1; y <= 64; y++) begin
-        isInside I(clock, x1,x2,x3,y1,y2,y3, x,y, S0);
-      end
-    end
-  end
+    input [11:0] p3X,
+    input [11:0] p3Y,
 
-  initial begin
-    $dumpvars();
-    #0 clock <= 0;
-    x1 = 12;
-    y1 = 15;
-    x2 = 12;
-    y2 = 25;
-    x3 = 15;
-    y3 = 15;
-    #10
+    input [11:0] pTX,
+    input [11:0] pTY,
+    output in
+    );
 
-  end
-endmodule //teste
+    wire sl1;
+    wire sl2;
+    wire sl3;
+    assign in = (sl1 == 1 && sl2 == 1 && sl3 == 1) ? 1 : 0;
+
+    sinal S1(p1X, p1Y, p2X, p2Y, pToX, pToY, sl1);
+    sinal S2(p2X, p2Y, p3X, p3Y, pToX, pToY, sl2);
+    sinal S3(p3X, p3Y, p1X, p1Y, pToX, pToY, sl3);
+
+endmodule
+
+module teste;
+    reg [11:0] p1X;
+    reg [11:0] p1Y;
+    reg [11:0] p2X;
+    reg [11:0] p2Y;
+    reg [11:0] p3X;
+    reg [11:0] p3Y;
+    reg [11:0] pToX;
+    reg [11:0] pToY;
+
+    wire In;
+
+    testeTri x(p1X, p1Y, p2X, p2Y, p3X, p3Y, pToX, pToY, In);
+ 
+    initial
+        begin
+           $dumpvars(0, x);
+           #1
+           p1X <= 20;
+           p1Y <= 20;
+           p2X <= 40;
+           p2Y <= 20;
+           p3X <= 30;
+           p3Y <= 40;
+           pToX <= 25;
+           pToY <= 25;
+
+           #1
+           pToX <= 25;
+           pToY <= 25;
+           #1
+           pToX <= 18;
+           pToY <= 25;
+           #1
+           pToX <= 20;
+           pToY <= 22;
+           #1
+           pToX <= 40;
+           pToY <= 22;
+           #40
+           $finish;
+        end
+endmodule
